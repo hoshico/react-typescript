20c7d763029e42fa271b1df8620e23e10f9fbb75
hoshico <77495217+hoshico@users.noreply.github.com>
2021-09-28 09:01:50 +0900

    add Practice3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Practice1 } from "./practice/Practice1";
+import { Practice2 } from "./practice/Practice2";
+import { Practice3 } from "./practice/Practice3";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <Practice1 />
+      <Practice2 />
+      <Practice3 />
     </div>
   );
 }
